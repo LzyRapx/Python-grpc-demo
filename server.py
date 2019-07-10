8c06c4f581526e00aa5a59bcb2cb83022df0de52
LzyRapx <1303377506@qq.com>
2019-07-11 00:45:03 +0800

    grpc server

--- server.py
+++ server.py
@@ -5,7 +5,23 @@ from concurrent import futures
 import time
 import hashlib
 import sqlite3
-class Server(object):
+
+import chatroom_pb2_grpc
+import chatroom_pb2 as proto
+
+class Server(chatroom_pb2_grpc.ChatRoomServicer):
     pass
+def serve():
+    print("Starting Server....")
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    chatroom_pb2_grpc.add_ChatRoomServicer_to_server(Server(),server)
+    server.add_insecure_port('[::]:9123')
+    server.start()
+    print("Server Started...Listen on [9123]")
+    try:
+        while True:
+            time.sleep(60 * 60)
+    except KeyboardInterrupt:
+        server.stop(0)
 if __name__ == "__main__":
-    pass
+    serve()
